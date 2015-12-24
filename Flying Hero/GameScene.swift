11ad5046c2b6ffe3178e9b9f5cb398fc3e8d1f5b
Quynh Nguyen <qn@rosslynanalytics.com>
2015-12-24 12:40:42 +0000

    Updated chapter 1

--- Flying Hero/GameScene.swift
+++ Flying Hero/GameScene.swift
@@ -10,12 +10,27 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var backgroundNode: SKSpriteNode?
+    var playerNode: SKSpriteNode?
+    
     required init?(coder aDecoder: NSCoder){
         super.init(coder: aDecoder)
     }
 
     override init(size: CGSize){
         super.init(size: size)
+        
         backgroundColor  = SKColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        
+        //adding background node
+        backgroundNode = SKSpriteNode(imageNamed: "Background")
+        backgroundNode!.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        backgroundNode!.position = CGPoint(x: size.width / 2.0, y: 0.0)
+        addChild(backgroundNode!)
+        
+        //add player node
+        playerNode = SKSpriteNode(imageNamed: "Player")
+        playerNode!.position = CGPoint(x: size.width / 2.0, y: size.height / 2.0)
+        addChild(playerNode!)
     }
 }
