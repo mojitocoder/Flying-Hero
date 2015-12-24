8a222d5044745131cf53cc28a6ebc78692df7a95
Quynh Nguyen <qn@rosslynanalytics.com>
2015-12-24 14:34:20 +0000

    Chapter 2 done

--- Flying Hero/GameScene.swift
+++ Flying Hero/GameScene.swift
@@ -18,19 +18,31 @@ class GameScene: SKScene {
     }
 
     override init(size: CGSize){
+        
         super.init(size: size)
+        
+        //Custom gravity for this game
+        physicsWorld.gravity = CGVectorMake(0.0, -0.1)
         
         backgroundColor  = SKColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
         
         //adding background node
         backgroundNode = SKSpriteNode(imageNamed: "Background")
         backgroundNode!.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        backgroundNode!.position = CGPoint(x: size.width / 2.0, y: 0.0)
+        backgroundNode!.position = CGPoint(x: size.width / 2.0, y: size.height / 2.0)
         addChild(backgroundNode!)
         
         //add player node
         playerNode = SKSpriteNode(imageNamed: "Player")
+        
+        playerNode!.physicsBody = SKPhysicsBody(circleOfRadius: playerNode!.size.width / 2)
+        playerNode!.physicsBody!.dynamic = true
+        
         playerNode!.position = CGPoint(x: size.width / 2.0, y: size.height / 2.0)
         addChild(playerNode!)
+        
+
+        print("Size of the screen is \(size.width) x \(size.height)")
+        print("Size of the background image is \(backgroundNode!.size.width) x \(backgroundNode!.size.height)")
     }
 }
