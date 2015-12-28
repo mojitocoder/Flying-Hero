647d57b3b6124ca2ddbf370f2e3baf1957356a3d
Quynh Nguyen <qn@rosslynanalytics.com>
2015-12-28 07:57:50 +0000

    Chapter 4

--- Flying Hero/GameScene.swift
+++ Flying Hero/GameScene.swift
@@ -12,12 +12,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var backgroundNode: SKSpriteNode?
     var playerNode: SKSpriteNode?
+    var foregroundNode: SKSpriteNode?
     var orbNode: SKSpriteNode?
+    
+    var ImpulseCount: Int = 5
     
     let CollisionCategoryPlayer: UInt32 = 0x1 << 1
     let CollisionCategoryPowerUpOrbs: UInt32 = 0x1 << 2
     
     let PowerUpNodeName: String = "PowerUpNode"
+    
     
     required init?(coder aDecoder: NSCoder){
         super.init(coder: aDecoder)
@@ -31,9 +35,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         //Custom gravity for this game
         physicsWorld.gravity = CGVectorMake(0.0, -1.0)
-        
         userInteractionEnabled = true
-        
         backgroundColor  = SKColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
         
         //adding background node
@@ -42,12 +44,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         backgroundNode!.position = CGPoint(x: size.width / 2.0, y: size.height / 2.0)
         addChild(backgroundNode!)
         
+        //foreground
+        foregroundNode = SKSpriteNode()
+        addChild(foregroundNode!)
+        
         //add player node
         playerNode = SKSpriteNode(imageNamed: "Player")
         playerNode!.physicsBody = SKPhysicsBody(circleOfRadius: playerNode!.size.width / 2)
 
-        playerNode!.physicsBody!.dynamic = true
-        playerNode!.position = CGPoint(x: size.width / 2.0, y: size.height / 2.0)
+        playerNode!.physicsBody!.dynamic = false
+        playerNode!.position = CGPoint(x: size.width / 2.0, y: size.height / 2.0 - 200)
         
         playerNode!.physicsBody!.linearDamping = 1.0
         playerNode!.physicsBody!.allowsRotation = false
@@ -55,19 +61,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         playerNode!.physicsBody!.contactTestBitMask = CollisionCategoryPowerUpOrbs
         playerNode!.physicsBody!.collisionBitMask = 0
         
-        addChild(playerNode!)
+        foregroundNode!.addChild(playerNode!)
         
         //add some orb nodes
-        orbNode = SKSpriteNode(imageNamed: "PowerUp")
-        orbNode!.position = CGPoint(x: size.width / 2.0 + 10, y: size.height - 200)
-        orbNode!.physicsBody = SKPhysicsBody(circleOfRadius: orbNode!.size.width / 2.0)
-        orbNode!.physicsBody!.dynamic = false
-        
-        orbNode!.physicsBody!.categoryBitMask = CollisionCategoryPowerUpOrbs
-        orbNode!.physicsBody!.collisionBitMask = 0
-        
-        orbNode!.name = PowerUpNodeName
-        addChild(orbNode!)
+        for i in 0...19 {
+            let orb = SKSpriteNode(imageNamed: "PowerUp")
+            orb.position = CGPointMake(size.width / 2, size.height / 2 + CGFloat(i * 150))
+            orb.physicsBody = SKPhysicsBody(circleOfRadius: orb.size.width / 2)
+            orb.physicsBody!.dynamic = false
+            orb.physicsBody!.categoryBitMask = CollisionCategoryPowerUpOrbs
+            orb.physicsBody!.collisionBitMask = 0
+            orb.name = PowerUpNodeName
+            
+            foregroundNode!.addChild(orb)
+        }
         
         //debug
         print("Size of the screen is \(size.width) x \(size.height)")
@@ -75,36 +82,36 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func didBeginContact(contact: SKPhysicsContact) {
+
         let nodeB = contact.bodyB.node!
-        
         print("Collide, nodeB = \(nodeB.name)")
 
         if nodeB.name == PowerUpNodeName {
             nodeB.removeFromParent()
+            ImpulseCount++
         }
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-    
-        //print("Touch")
-
-        playerNode!.physicsBody!.applyImpulse(CGVectorMake(0.0, 20.0))
-        /* Called when a touch begins */
         
-//        for touch in touches {
-//            let location = touch.locationInNode(self)
-//            
-//            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-//            
-//            sprite.xScale = 0.5
-//            sprite.yScale = 0.5
-//            sprite.position = location
-//            
-//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-//            
-//            sprite.runAction(SKAction.repeatActionForever(action))
-//            
-//            self.addChild(sprite)
-//        }
+        //use the first user's touch to start the game
+        if !playerNode!.physicsBody!.dynamic {
+            playerNode!.physicsBody!.dynamic = true;
+        }
+        
+        //print("Touch")
+        if ImpulseCount > 0 {
+            playerNode!.physicsBody!.applyImpulse(CGVectorMake(0.0, 20.0))
+            ImpulseCount--
+        }
+    }
+    
+    let ScrollThreshold: CGFloat = 360.0
+    
+    override func update(currentTime: NSTimeInterval) {
+        if playerNode!.position.y >= ScrollThreshold {
+            backgroundNode!.position = CGPointMake(backgroundNode!.position.x, -(playerNode!.position.y - ScrollThreshold)/8)
+            foregroundNode!.position = CGPointMake(foregroundNode!.position.x, -(playerNode!.position.y - ScrollThreshold))
+        }
     }
 }
